eea288e32c7a5bc95ddf83f5ee83493960ab66db
amoljp19 <41947289+amoljp19@users.noreply.github.com>
2022-10-14 18:56:03 +0530

    binary tree node and binary trees diagram

---
+++ app/src/main/java/com/softaai/dsa_kotlin/tree/binarytree/BinaryNode.kt
@@ -0,0 +1,33 @@
+package com.softaai.dsa_kotlin.tree.binarytree
+
+
+/**
+ * Created by amoljp19 on 10/14/2022.
+ * softAai Apps.
+ */
+
+typealias Visitor<T> = (T) -> Unit
+class BinaryNode<T>(val value: T) {
+
+    var leftChild: BinaryNode<T>? = null
+    var rightChild: BinaryNode<T>? = null
+
+
+    override fun toString() = diagram(this)
+    private fun diagram(node: BinaryNode<T>?,
+                        top: String = "",
+                        root: String = "",
+                        bottom: String = ""): String {
+        return node?.let {
+            if (node.leftChild == null && node.rightChild == null) {
+                "$root${node.value}\n"
+            } else {
+                diagram(node.rightChild, "$top ", "$top┌──", "$top│ ") +
+                        root + "${node.value}\n" + diagram(node.leftChild,
+                    "$bottom│ ", "$bottom└──", "$bottom ")
+
+            }
+        } ?: "${root}null\n"
+    }
+
+}
---
+++ app/src/main/java/com/softaai/dsa_kotlin/tree/binarytree/BinaryTreeRunner.kt
@@ -0,0 +1,28 @@
+package com.softaai.dsa_kotlin.tree.binarytree
+
+
+/**
+ * Created by amoljp19 on 10/14/2022.
+ * softAai Apps.
+ */
+
+fun main(){
+
+    val zero = BinaryNode(0)
+    val one = BinaryNode(1)
+    val five = BinaryNode(5)
+    val seven = BinaryNode(7)
+    val eight = BinaryNode(8)
+    val nine = BinaryNode(9)
+
+    seven.leftChild = one
+    one.leftChild = zero
+    one.rightChild = five
+    seven.rightChild = nine
+    nine.leftChild = eight
+
+    val tree = seven
+
+    println(tree)
+
+}
