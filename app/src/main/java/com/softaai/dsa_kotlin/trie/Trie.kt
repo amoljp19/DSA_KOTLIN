156dfd1bec5ce197ef01733818f3d76b88fb8ae6
amoljp19 <41947289+amoljp19@users.noreply.github.com>
2022-11-07 15:28:18 +0530

    tries initial implementation

---
+++ app/src/main/java/com/softaai/dsa_kotlin/trie/Trie.kt
@@ -0,0 +1,13 @@
+package com.softaai.dsa_kotlin.trie
+
+
+/**
+ * Created by amoljp19 on 11/7/2022.
+ * softAai Apps.
+ */
+class Trie {
+
+    private val root = TrieNode(key = null, parent = null)
+
+    
+}
---
+++ app/src/main/java/com/softaai/dsa_kotlin/trie/TrieNode.kt
@@ -0,0 +1,12 @@
+package com.softaai.dsa_kotlin.trie
+
+
+/**
+ * Created by amoljp19 on 11/7/2022.
+ * softAai Apps.
+ */
+class TrieNode<Key>(var key : Key?, var parent: TrieNode<Key>?) {
+    val children: HashMap<Key, TrieNode<Key>> = HashMap()
+
+    val isTerminating = false
+}
