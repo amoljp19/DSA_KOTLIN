5878b82cd8930670cc39568e3676bc648c471c3d
amoljp19 <41947289+amoljp19@users.noreply.github.com>
2022-11-18 19:25:08 +0530

    military and seniority based sorting concert example

--- app/src/main/java/com/softaai/dsa_kotlin/priorityqueue/PriorityQueueRunner.kt
+++ app/src/main/java/com/softaai/dsa_kotlin/priorityqueue/PriorityQueueRunner.kt
@@ -49,7 +49,7 @@ fun main(){
     }*/
 
 
-    "custom sorting array list priority queue" example {
+   /* "custom sorting array list priority queue" example {
         val customSortingArrayListPriorityQueue = CustomSortingArrayListPriorityQueue<Int>()
 
         arrayListOf(1, 12, 3, 4, 1, 6, 8, 7).forEach {
@@ -64,7 +64,65 @@ fun main(){
             println(customSortingArrayListPriorityQueue.dequeue())
         }
 
+    }*/
+
+
+    "concert line below military" example {
+        val p1 = Person("Josh", 21, true)
+        val p2 = Person("Jake", 22, true)
+        val p3 = Person("Clay", 28, false)
+        val p4 = Person("Cindy", 28, false)
+        val p5 = Person("Sabrina", 30, false)
+
+        val priorityQueue = ComparatorPriorityQueueImpl<Person>(militaryComparator)
+
+        arrayListOf<Person>(p1, p2, p3, p4, p5).forEach {
+            priorityQueue.enqueue(it)
+        }
+
+        while(!priorityQueue.isEmpty){
+            println(priorityQueue.dequeue()?.name)
+        }
+
     }
 
+
+}
+
+
+
+
+/*
+Your favorite concert was sold out. Fortunately, there’s a waitlist for people who still
+want to go. However, the ticket sales will first prioritize someone with a military
+background, followed by seniority
+
+Given a list of people on the waitlist, you would like to prioritize the people in the
+following order:
+1. Military background.
+2. Seniority, by age
+*/
+
+data class Person(
+    val name: String,
+    val age : Int,
+    val isMilitary : Boolean
+)
+
+
+object militaryComparator : Comparator<Person>{
+
+    override fun compare(p1: Person, p2: Person): Int {
+        if(p1.isMilitary && !p2.isMilitary){
+            return 1
+        }
+        else if (!p1.isMilitary && p2.isMilitary){
+            return -1
+        }
+        else if (p1.isMilitary && p2.isMilitary){
+            return p1.age.compareTo(p2.age)
+        }
+        return 0
+    }
 
 }
