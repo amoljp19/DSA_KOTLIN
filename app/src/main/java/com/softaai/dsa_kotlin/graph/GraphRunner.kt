cf1eb77e5e38685ce91829cd32a9b65f3dbbbcff
amoljp19 <41947289+amoljp19@users.noreply.github.com>
2022-11-28 17:30:25 +0530

    in real time getting routes and prices examples

--- app/src/main/java/com/softaai/dsa_kotlin/graph/GraphRunner.kt
+++ app/src/main/java/com/softaai/dsa_kotlin/graph/GraphRunner.kt
@@ -7,31 +7,39 @@ package com.softaai.dsa_kotlin.graph
  */
 fun main(){
 
-    val adjacencyGraph = AdjacencyList<String>()
+    val adjacencyListGraph = AdjacencyList<String>()
 
-    val singapore = adjacencyGraph.createVertex("Singapore")
-    val hongKong = adjacencyGraph.createVertex("Hong Kong")
-    val tokyo = adjacencyGraph.createVertex("Tokyo")
-    val detroit = adjacencyGraph.createVertex("Detroit")
-    val austin = adjacencyGraph.createVertex("Austin, Texas")
-    val washington = adjacencyGraph.createVertex("Washington, DC")
-    val seattle = adjacencyGraph.createVertex("Seattle")
-    val sanfrancisco = adjacencyGraph.createVertex("San Francisco")
+    val singapore = adjacencyListGraph.createVertex("Singapore")
+    val hongKong = adjacencyListGraph.createVertex("Hong Kong")
+    val tokyo = adjacencyListGraph.createVertex("Tokyo")
+    val detroit = adjacencyListGraph.createVertex("Detroit")
+    val austin = adjacencyListGraph.createVertex("Austin, Texas")
+    val washington = adjacencyListGraph.createVertex("Washington, DC")
+    val seattle = adjacencyListGraph.createVertex("Seattle")
+    val sanfrancisco = adjacencyListGraph.createVertex("San Francisco")
 
-    adjacencyGraph.add(EdgeType.UNDIRECTED, singapore, hongKong, 300.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, singapore, tokyo, 500.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, hongKong, tokyo, 250.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, tokyo, detroit, 450.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, tokyo, washington,300.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, hongKong, sanfrancisco, 600.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, detroit, austin, 50.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, austin, washington,292.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, sanfrancisco, washington, 337.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, washington, seattle, 277.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, sanfrancisco, seattle, 218.0)
-    adjacencyGraph.add(EdgeType.UNDIRECTED, austin, sanfrancisco,297.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, singapore, hongKong, 300.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, singapore, tokyo, 500.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, hongKong, tokyo, 250.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, tokyo, detroit, 450.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, tokyo, washington,300.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, hongKong, sanfrancisco, 600.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, detroit, austin, 50.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, austin, washington,292.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, sanfrancisco, washington, 337.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, washington, seattle, 277.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, sanfrancisco, seattle, 218.0)
+    adjacencyListGraph.add(EdgeType.UNDIRECTED, austin, sanfrancisco,297.0)
 
-    println(adjacencyGraph)
+    //println(adjacencyGraph)
+
+   // println("How much from tokyo to singapore : ${adjacencyGraph.weight(tokyo, singapore)}")
+
+    println("sanfrancisco outgoing flights")
+    println("------------------------------")
+    adjacencyListGraph.edges(sanfrancisco).forEach {edges ->
+        println("from : ${edges.source.data} to : ${edges.destination.data}")
+    }
 
 
 }
